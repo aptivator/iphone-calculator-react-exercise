baac84648f6201162c0e7cd40d46645f79de7b02
Dmitriy Nesterkin <dmitriy.nesterkin@gmail.com>
2025-06-26 13:15:26 -0400

    Add example files

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import App          from './App.jsx'
-
-let rootEl = document.getElementById('root');
-createRoot(rootEl).render(<StrictMode><App /></StrictMode>);
